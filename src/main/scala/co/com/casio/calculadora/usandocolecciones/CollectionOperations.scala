118e6e82bf7f6fb5a88f7bebc3579f04d0603846
juanftech <juan.restrepo@techandsolve.com>
2018-04-05 11:00:37 -0500

    Operaciones con colecciones en scala

---
+++ src/main/scala/co/com/casio/calculadora/usandocolecciones/CollectionOperations.scala
@@ -0,0 +1,48 @@
+package co.com.casio.calculadora.usandocolecciones
+
+object CollectionOperations extends App {
+
+  val source = io.Source.fromFile("E:/Anarchy_Linux/Calculadora/ArchivoALeer.txt")
+  val bufferedSourceToList: List[String] = {
+    val list = source.getLines().toList
+    source.close()
+    list
+  }
+
+  def giveMePlayers(list: List [String]): List[Player] = list match {
+    case head :: tail => tail map {line =>
+      val columns = line.split((",")).map (_.trim)
+      Player(columns(5),columns(6),columns(9),columns(7),columns(8),columns(10), columns(12), columns(0),columns(14))
+    }
+    case Nil => List[Player]()
+  }
+
+  val players = giveMePlayers(bufferedSourceToList)
+  players foreach println
+
+  def showPlayers (players: List[Player]) = players.foreach {p =>
+    println(s"""Player: ${p.name}   Country: ${p.nationality}     Ranking 2016: ${p.ranking2016}
+
+
+      ************************** Other Information ***********************
+      Age: ${p.age} | Club: ${p.club} | Domestic League: ${p.domesticLeague}
+      Raw Total: ${p.rawTotal}  | Final Score: ${p.finalScore}  | Ranking 2015: ${p.ranking2015}
+      #####################################################################""")
+  }
+  showPlayers(players)
+
+  /**
+    * Los metodos de listas y stream takeWhile y dropWhile son utiles asi:
+    * El takeWhile metodo toma la subcoleccion mas larga de elementos que
+    * satisface el predicado.
+    * El dropWhile metodo elimina la subcollecion mas larga de elementos que
+    * satisface el predicado ASi:
+    * Para listar los diez mejores jugadores (con ranking menor a 11)
+    */
+
+  val takeTop10 = players takeWhile(_.ranking2015.toInt < 11)
+  println(s"LOS 10 MEJORES JUGADORES SON: ${takeTop10 foreach println}")
+
+}
+
+case class Player (name: String, nationality: String, age: String, club: String, domesticLeague: String, rawTotal: String, finalScore: String, ranking2016: String, ranking2015: String)
