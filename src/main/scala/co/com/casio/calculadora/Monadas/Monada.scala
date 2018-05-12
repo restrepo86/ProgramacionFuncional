cb88bbddabbba10ebbf968239cec358ab4378ac7
juanftech <juan.restrepo@techandsolve.com>
2018-05-12 10:33:37 -0500

    Ejercicio con monadas hecho en clase

--- src/main/scala/co/com/casio/calculadora/Monadas/Monada.scala
+++ src/main/scala/co/com/casio/calculadora/Monadas/Monada.scala
@@ -39,6 +39,26 @@ object Monad {
                      (f:A => Try[B]): Try[B] = fa flatMap f
   }
 
-  
+  case class OptionT[F[_], A](value: F[Option[A]])
+
+  implicit def optionTMonad[F[_]](implicit F: Monad[F]) = {
+    new Monad[OptionT[F, ?]] {
+      def pure[A](a: A): OptionT[F, A] = OptionT(F.pure(Some(a)))
+      def flatMap[A, B](fa: OptionT[F, A])(f: A => OptionT[F, B]): OptionT[F, B] =
+        OptionT {
+          F.flatMap(fa.value) {
+            case None => F.pure(None)
+            case Some(a) => f(a).value
+          }
+        }
+
+      def defaultTailRecM[A](a: A) = ???
+
+      def tailRecM[A, B](a: A)(f: A => OptionT[F, Either[A, B]]): OptionT[F, B] =
+        defaultTailRecM(a)(f)
+    }
+  }
+
+
 
 }
